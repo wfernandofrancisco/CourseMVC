d4d044bc19c63884565c94228b520b91a3211a47
wfernandofrancisco <wfernandofrancisco@gmail.com>
2022-07-25 15:51:01 -0300

    Adaptando MYSQL e Migration - Primeira Migracao

--- CourseMVC/Startup.cs
+++ CourseMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace CourseMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<CourseMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CourseMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("CourseMVCContext"), builder =>
+                        builder.MigrationsAssembly("CourseMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
