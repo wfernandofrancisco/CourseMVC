70eb761c12d26850cb80cea282e3eea1b7b209b8
wfernandofrancisco <wfernandofrancisco@gmail.com>
2022-07-25 15:15:21 -0300

    Crud Department

---
+++ CourseMVC/Data/CourseMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseMVC.Models
+{
+    public class CourseMVCContext : DbContext
+    {
+        public CourseMVCContext (DbContextOptions<CourseMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CourseMVC.Models.Department> Department { get; set; }
+    }
+}
--- CourseMVC/Startup.cs
+++ CourseMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CourseMVC.Models;
 
 namespace CourseMVC
 {
@@ -33,6 +35,9 @@ namespace CourseMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CourseMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CourseMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
