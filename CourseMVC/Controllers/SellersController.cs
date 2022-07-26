9bbcb2480ac3630b354484b6fd198e230139bb94
wfernandofrancisco <wfernandofrancisco@gmail.com>
2022-07-26 16:08:53 -0300

    Simple Create Seller

--- CourseMVC/Controllers/SellersController.cs
+++ CourseMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
-﻿using CourseMVC.Services;
+﻿using CourseMVC.Models;
+using CourseMVC.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -20,5 +21,19 @@ namespace CourseMVC.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
