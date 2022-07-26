90a4d59528793c03924f97bb5b59b0d816c85d5c
wfernandofrancisco <wfernandofrancisco@gmail.com>
2022-07-26 15:01:47 -0300

    Criando classe para popular o banco de dados

--- CourseMVC/Data/CourseMVCContext.cs
+++ CourseMVC/Data/CourseMVCContext.cs
@@ -13,6 +13,8 @@ namespace CourseMVC.Models
         {
         }
 
-        public DbSet<CourseMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
     }
 }
--- CourseMVC/Startup.cs
+++ CourseMVC/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using CourseMVC.Models;
+using CourseMVC.Data;
 
 namespace CourseMVC
 {
@@ -39,14 +40,17 @@ namespace CourseMVC
             services.AddDbContext<CourseMVCContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("CourseMVCContext"), builder =>
                         builder.MigrationsAssembly("CourseMVC")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
