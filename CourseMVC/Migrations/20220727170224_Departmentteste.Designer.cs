8229bbd02f362f13574816028184ee0c3ae76902
wfernandofrancisco <wfernandofrancisco@gmail.com>
2022-07-27 14:13:48 -0300

    Criando chave estrangeira

--- CourseMVC/Migrations/20220727170224_Departmentteste.Designer.cs
+++ CourseMVC/Migrations/20220727170224_Departmentteste.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CourseMVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CourseMVC.Migrations
 {
     [DbContext(typeof(CourseMVCContext))]
-    partial class CourseMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20220727170224_Departmentteste")]
+    partial class Departmentteste
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
