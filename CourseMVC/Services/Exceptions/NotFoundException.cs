19460f396706f0a329aaca4ec1b791024df771e5
wfernandofrancisco <wfernandofrancisco@gmail.com>
2022-08-01 14:37:24 -0300

    Update seller and custom service exception

---
+++ CourseMVC/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CourseMVC.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message): base(message)
+        {
+        }
+
+
+    }
+}
--- CourseMVC/Services/SellerService.cs
+++ CourseMVC/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using CourseMVC.Services.Exceptions;
 
 namespace CourseMVC.Services
 {
@@ -37,6 +38,23 @@ namespace CourseMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
